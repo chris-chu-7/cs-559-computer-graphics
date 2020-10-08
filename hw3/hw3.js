689f1b61166a78c5a952d6b87385356e8578ca74
Chris Chu <42784131+wanderingessence@users.noreply.github.com>
2020-10-08 19:13:31 -0400

    implemented the skeleton code

--- hw3/hw3.js
+++ hw3/hw3.js
@@ -1,11 +1,35 @@
-function time(){
-  
-    var date = new Date();
-    var canvas = document.getElementById('canvas');
+function setup() {
+    var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
     
-    
-    
-    
-  }
+    context.scale(0.4, 0.4)
   
+    function draw() {
+	canvas.width = canvas.width;
+
+	
+	function setCanvasTransform(Tx) {
+	    context.setTransform(Tx[0],Tx[1],Tx[3],Tx[4],Tx[6],Tx[7]);
+	}
+
+	function circle(color) {
+      context.scale(0.4, 0.4)
+      context.strokeStyle = color;
+	   context.beginPath();
+       context.arc(175, 175, 400, 0, Math.PI * 2, true);
+       context.stroke();
+	}
+	
+      
+      var TCircle = mat3.create();
+      mat3.fromTranslation(TCircle,[150,150]);
+      setCanvasTransform(TCircle);
+      circle("blue");
+
+
+    }
+
+
+    draw();
+}
+window.onload = setup;
