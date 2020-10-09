53a685ddd910a72b3fa0978910d385b75539a3da
Chris Chu <42784131+wanderingessence@users.noreply.github.com>
2020-10-08 22:02:32 -0400

    Update hw3.js

--- hw3/hw3.js
+++ hw3/hw3.js
@@ -1,11 +1,16 @@
 function setup() {
+    var date = new Date();
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
 
-    context.scale(0.4, 0.4)
 
     function draw() {
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var seconds = date.getSeconds();
+
         canvas.width = canvas.width;
+
 
 
         function setCanvasTransform(Tx) {
@@ -13,7 +18,6 @@ function setup() {
         }
 
         function circle(color) {
-            context.scale(0.4, 0.4)
             context.strokeStyle = color;
             context.beginPath();
             context.arc(175, 175, 400, 0, Math.PI * 2, true);
@@ -21,7 +25,7 @@ function setup() {
         }
 
         function markers(color) {
-            context.scale(0.4, 0.4)
+            context.lineWidth = 3;
             context.strokeStyle = color;
             for (var j = 0; j < 60; j++) {
                 if (j % 5 !== 0) {
@@ -34,17 +38,54 @@ function setup() {
             }
         }
 
+        function secondHand(color) {
+            context.strokeStyle = 'orange';
+            context.lineWidth = 6;
+            context.beginPath();
+            context.moveTo(-30, 0);
+            context.lineTo(320, 0);
+            context.stroke();
+        }
+
+        function minuteHand(color) {
+            context.strokeStyle = 'red';
+
+            context.lineWidth = 10;
+            context.beginPath();
+            context.moveTo(-20, 0);
+            context.lineTo(300, 0);
+            context.stroke();
+
+        }
+
+        function hourHand(color) {
+
+        }
+
+
+
 
         var TCircle = mat3.create();
         mat3.fromTranslation(TCircle, [150, 150]);
+        mat3.scale(TCircle, TCircle, [0.4, 0.4]);
         setCanvasTransform(TCircle);
         circle("blue");
 
+
         var TMarker = mat3.create();
-        mat3.fromTranslation(TMarker, [70, 70]);
+        mat3.fromTranslation(TMarker, [170, 170]);
         mat3.multiply(TMarker, TCircle, TMarker);
         setCanvasTransform(TMarker);
         markers("green");
+
+
+        var TsecondHand = mat3.create();
+        mat3.rotate(TsecondHand, TsecondHand, seconds * (Math.PI / 30));
+        secondHand("orange");
+
+        var TminuteHand = mat3.create();
+        mat3.rotate(TminuteHand, TminuteHand, minutes * (Math.PI / 30) + seconds * (Math.PI / (30 * 60)));
+        minuteHand("red");
 
 
 
@@ -53,5 +94,8 @@ function setup() {
 
 
     draw();
+    window.requestAnimationFrame(time);
 }
+
 window.onload = setup;
+window.requestAnimationFrame(time);
