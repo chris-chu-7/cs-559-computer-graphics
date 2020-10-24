058a3f5f661f89af58bb757d30675d86bbae8f2e
Chris Chu <42784131+wanderingessence@users.noreply.github.com>
2020-10-24 08:59:53 -0400

    about to go

--- hw4/hw4.js
+++ hw4/hw4.js
@@ -3,16 +3,26 @@ function setup(){
     var context = canvas.getContext('2d');
     var slider = document.getElementById('path');
     slider.value = 0;
+
+    function setCanvasTransform(Tx) {
+        context.setTransform(Tx[0], Tx[1], Tx[3], Tx[4], Tx[6], Tx[7]);
+    }
     
     function drawCar(color){
         context.fillStyle = color;
         context.beginPath();
-        context.rect(20, 20, 60, 100);
+        context.lineTo(10, 50);
+        context.lineTo(10, 0);
+        context.lineTo(40, 0);
+        context.lineTo(40, 50);
         context.stroke();
         context.fill();
     }
 
     function draw(){
+        var car = mat3.create();
+        mat3.fromTranslation(car, [500, 900]);
+        setCanvasTransform(car);
         drawCar('rgb(255,20,147)');
     }
 
