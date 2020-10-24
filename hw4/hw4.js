78814ddc4bded110b50ba61eedc44a2dadcfd792
Chris Chu <42784131+wanderingessence@users.noreply.github.com>
2020-10-24 08:32:22 -0400

    rectangle drew successfully
    rectangle drew successfully

--- hw4/hw4.js
+++ hw4/hw4.js
@@ -4,6 +4,18 @@ function setup(){
     var slider = document.getElementById('path');
     slider.value = 0;
     
+    function drawCar(){
+        context.beginPath();
+        context.rect(20, 20, 150, 100);
+        context.stroke();
+        context.fill();
+    }
+
+    function draw(){
+        drawCar();
+    }
+
+    draw();
   }
   
   window.onload = setup();
