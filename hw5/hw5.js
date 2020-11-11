5ca5d88151904c587369c0c4853d1da62faf5f50
Chris Chu <42784131+wanderingessence@users.noreply.github.com>
2020-11-11 17:44:06 -0500

    Update hw5.js

--- hw5/hw5.js
+++ hw5/hw5.js
@@ -16,15 +16,15 @@ function setup() {
         cameraCanvas.width = cameraCanvas.width;
 
         var tParam = curveSlider.value * 0.01;
-        var viewAngle = rotateSlider.value * 0.429 * Math.PI;
+        var viewAngle = rotateSlider.value * 0.005 * Math.PI;
 
-        function moveToTx(){
+        function moveToTx(loc,Tx){
             var res = vec3.create();
             vec3.transformMat4(res, loc, Tx);
             context.moveTo(res[0], res[1]);
         }
 
-        function lineToTx(){
+        function lineToTx(loc, Tx){
             var res = vec3.create();
             vec3.transformMat4(res, loc, Tx);
             context.lineTo(res[0], res[1]);
@@ -171,6 +171,42 @@ function setup() {
             return result;
         }
 
+        var p0=[0,0,0];
+	var d0=[100,300,0];
+	var p1=[100,100,0];
+	var d1=[-100,300,0];
+	var p2=[200,200,0];
+	var d2=[0,300,0];
+
+	var P0 = [p0,d0,p1,d1]; // First two points and tangents
+	var P1 = [p1,d1,p2,d2]; // Last two points and tangents
+
+	var C0 = function(t_) {return Cubic(Hermite,P0,t_);};
+	var C1 = function(t_) {return Cubic(Hermite,P1,t_);};
+
+	var C0prime = function(t_) {return Cubic(HermiteDerivative,P0,t_);};
+	var C1prime = function(t_) {return Cubic(HermiteDerivative,P1,t_);};
+      
+    var Ccomp = function(t) {
+        if (t<1){
+            var u = t;
+            return C0(u);
+        } else {
+            var u = t-1.0;
+            return C1(u);
+        }          
+	}
+
+    var Ccomp_tangent = function(t) {
+        if (t<1){
+            var u = t;
+            return C0prime(u);
+        } else {
+            var u = t-1.0;
+            return C1prime(u);
+        }          
+	}
+
         var CameraCurve = function(angle) {
             var distance = 120.0;
             var eye = vec3.create();
@@ -190,6 +226,75 @@ function setup() {
             }
             context.stroke();
         }
+
+        var eyeCamera = CameraCurve(viewAngle);
+        var origin = vec3.fromValues(0, 0, 0); 
+        var verticalY = vec3.fromValues(0, 100, 0);
+        var TlookAtCamera = mat4.create();
+        mat4.lookAt(TlookAtCamera, eyeCamera, origin, verticalY);
+
+        var eyeObserver = vec3.fromValues(500, 300, 500);
+        var targetObserver = vec3.fromValues(0, 50, 0);
+        var upObserver = vec3.fromValues(0, 1, 0);
+        var TlookAtObserver = mat4.create();
+        mat4.lookAt(TlookAtObserver, eyeObserver, targetObserver, upObserver);
+
+          // Create ViewPort transform (assumed the same for both canvas instances)
+    var Tviewport = mat4.create();
+	mat4.fromTranslation(Tviewport,[200,300,0]);  // Move the center of the
+                                                  // "lookAt" transform (where
+                                                  // the camera points) to the
+                                                  // canvas coordinates (200,300)
+	mat4.scale(Tviewport,Tviewport,[100,-100,1]); // Flip the Y-axis,
+                                                  // scale everything by 100x
+    // make sure you understand these    
+
+    context = cameraContext;
+
+    // Create Camera projection transform
+    // (orthographic for now)
+    var TprojectionCamera = mat4.create();
+    mat4.ortho(TprojectionCamera,-100,100,-100,100,-1,1);
+    //mat4.perspective(TprojectionCamera,Math.PI/4,1,-1,1); // Use for perspective teaser!
+
+    // Create Observer projection transform
+    // (orthographic for now)
+    var TprojectionObserver = mat4.create();
+    mat4.ortho(TprojectionObserver,-120,120,-120,120,-1,1);
+     
+    // Create transform t_VP_PROJ_CAM that incorporates
+    // Viewport, projection and camera transforms
+    var tVP_PROJ_VIEW_Camera = mat4.create();
+    mat4.multiply(tVP_PROJ_VIEW_Camera,Tviewport,TprojectionCamera);
+    mat4.multiply(tVP_PROJ_VIEW_Camera,tVP_PROJ_VIEW_Camera,TlookAtCamera);
+    var tVP_PROJ_VIEW_Observer = mat4.create();
+    mat4.multiply(tVP_PROJ_VIEW_Observer,Tviewport,TprojectionObserver);
+    mat4.multiply(tVP_PROJ_VIEW_Observer,tVP_PROJ_VIEW_Observer,TlookAtObserver);
+      
+	// Create model(ing) transform
+    // (from moving object to world)
+    var Tmodel = mat4.create();
+	mat4.fromTranslation(Tmodel,Ccomp(tParam));
+    var tangent = Ccomp_tangent(tParam);
+    var angle = Math.atan2(tangent[1],tangent[0]);
+	mat4.rotateZ(Tmodel,Tmodel,angle);
+
+    // Create transform t_VP_PROJ_VIEW_MOD that incorporates
+    // Viewport, projection, camera, and modeling transform
+    var tVP_PROJ_VIEW_MOD_Camera = mat4.create();
+	mat4.multiply(tVP_PROJ_VIEW_MOD_Camera, tVP_PROJ_VIEW_Camera, Tmodel);
+    var tVP_PROJ_VIEW_MOD1_Observer = mat4.create();
+	mat4.multiply(tVP_PROJ_VIEW_MOD1_Observer, tVP_PROJ_VIEW_Observer, Tmodel);
+    var tVP_PROJ_VIEW_MOD2_Observer = mat4.create();
+    mat4.translate(tVP_PROJ_VIEW_MOD2_Observer, tVP_PROJ_VIEW_Observer, eyeCamera);
+	var TlookFromCamera = mat4.create();
+    mat4.invert(TlookFromCamera,TlookAtCamera);
+    mat4.multiply(tVP_PROJ_VIEW_MOD2_Observer, tVP_PROJ_VIEW_MOD2_Observer, TlookFromCamera);
+
+    context = cameraContext;
+    draw3DAxes("grey",tVP_PROJ_VIEW_Camera,100.0);
+
+
 
 
 
