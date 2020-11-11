1dbb35e15db386a27f9d038f2d2a656e163ee8e4
Chris Chu <42784131+wanderingessence@users.noreply.github.com>
2020-11-11 10:40:11 -0500

    Update hw5.js

--- hw5/hw5.js
+++ hw5/hw5.js
@@ -7,6 +7,40 @@ function setup() {
     var rotateSlider = document.getElementById('slider2');
     curveSlider.value = 0;
     rotateSlider.value = 0; 
+
+    var context = cameraContext;
+
+    function draw(){
+        
+        observerCanvas.width = observerCanvas.width;
+        cameraCanvas.width = cameraCanvas.width;
+
+        var tParam = curveSlider.value * 0.01;
+        var viewAngle = rotateSlider.value * 0.429 * Math.PI;
+
+        function moveToTx(){
+            var res = vec3.create();
+            vec3.transformMat4(res, loc, Tx);
+            context.moveTo(res[0], res[1]);
+        }
+
+        function lineToTx(){
+            var res = vec3.create();
+            vec3.transformMat4(res, loc, Tx);
+            context.lineTo(res[0], res[1]);
+        }
+
+
+    }
+
+    draw();
+    curveSlider.addEventListener("input", draw);
+    rotateSlider.addEventListener("input", draw);
+    
+
+
+
+
 }
 
 window.onload = setup;
