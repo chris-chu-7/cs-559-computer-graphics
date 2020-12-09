13e278de165758d13d5e4f00e5173f9214c69106
Chris Chu <42784131+wanderingessence@users.noreply.github.com>
2020-12-09 16:56:47 -0500

    completed instruction set.

--- hw7/hw7.js
+++ hw7/hw7.js
@@ -1,7 +1,29 @@
 //Assignment 7 Requirements: 
 
 /* 
+* Write a Vertex Shader
+* Write a Fragment Shader
+* Compile and link these Shaders
+* Define your own vertex attributes
+*   - Pointers Query the data
+*   - Defining Associated Data
+*   - Dispatch data to GPU
+*   - Any other necessary operation
+*   - Define geometry via trangles/shapes 
+    and buffer data to the GPU
+*   - Get all the transform into uniforms
+*   - Load Texture Images
+*   - Use Z-Buffer Visibility mechanism
+*   - Polyhedral Nonflat object please!
+*   - Use diffuse/specular shading. 
+*   - Some way to change the scene rather than just
+    -rotation.
+*   - Spin a camera, and properly place this camera
 
-
-
+* BONUS:  
+*   - Non-trivial lighting + texturing 
+*   - Model objects
+*   - Multiple textures
+*   - Comple objects
+*   - Interesting camera motion
 */
